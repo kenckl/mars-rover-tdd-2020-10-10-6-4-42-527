c269562598afb5c1f5b5e5bc75aadb66ccdb89ab
kenckl <klcheung8355@gmail.com>
2020-11-26 18:26:16 +0800

    [MarsRover][Ken] Implemented test case 0 0 S R

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -77,4 +77,18 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("E", marsRover.getDirection());
     }
+
+    @Test
+    public void should_return_0_0_W_when_executeCommands_given_0_0_S_and_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0,0,"S");
+
+        //when
+        marsRover.executeCommands("R");
+
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getDirection());
+    }
 }
