1f59cccc63f8050ec0c47f55d83ce4071210dbc2
kenckl <klcheung8355@gmail.com>
2020-12-01 18:09:04 +0800

    [MarsRover][Ken] Created a TurnRightCommand Class

---
+++ src/main/java/com/afs/tdd/TurnRightCommand.java
@@ -0,0 +1,14 @@
+package com.afs.tdd;
+
+public class TurnRightCommand implements Command {
+    private CommandCenter commandCenter;
+
+    public TurnRightCommand(CommandCenter commandCenter){
+        this.commandCenter = commandCenter;
+    }
+
+    @Override
+    public void execute(){
+        commandCenter.goLeft();
+    }
+}
